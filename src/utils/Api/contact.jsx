c9613628c031e5ab4436aa456be9973621425f4f
tushar756 <112198307+tushar756@users.noreply.github.com>
2024-02-20 21:33:54 +0530

    asdfads

--- src/utils/Api/contact.jsx
+++ src/utils/Api/contact.jsx
@@ -1,4 +1,4 @@
 import axios from 'axios'
 export default axios.create({
-    baseURL:"https://harmony-tickets-backend.vercel.app/api/"
+    baseURL:"https://harmony-tickets-backend.vercel.app/api"
 })
