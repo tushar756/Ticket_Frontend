6e5961112141a7c4cc29258386024fb3aa97bad5
tushar756 <112198307+tushar756@users.noreply.github.com>
2024-02-24 14:02:58 +0530

    Dashboard Complete

--- src/components/pages/Staff/Dashboard/StaffDashboard.jsx
+++ src/components/pages/Staff/Dashboard/StaffDashboard.jsx
@@ -8,75 +8,105 @@ import frame4 from "../../../../assets/frames/Frame 4.png";
 import frame5 from "../../../../assets/frames/Frame 5.png";
 // import frame6 from "../../../assets/frames/Frame 6.png";
 import "./StaffDashboard.scss";
-const StaffDashboard = () => {
+import { NavLink } from "react-router-dom";
 
+
+const StaffDashboard = () => {
   const [count, setCount] = useState({
-    Pending: 0,
-    Resolved: 0,
-    Open: 0,
-    pendingMoreThanTwoDaysCount: 0,
-    staffCount: 0
+    pendingCount: 0,
+    resolvedCount: 0,
+    openCount: 0,
+    HighPriorityCount: 0,
+    LowPriorityCount: 0,
+    MidPriorityCount: 0,
   });
+  const [loading, setLoading] = useState(true);
   const user = JSON.parse(localStorage.getItem("user"));
+
   const getAllTickets = async () => {
     try {
       const response = await API.get("/staff/counts", {
         user: user._id,
       });
-      
- 
+
       setCount(response.data.data);
+      setLoading(false);
     } catch (error) {
       console.error("Error fetching data:", error);
-      // setData([]);
     }
   };
-
 
   useEffect(() => {
     getAllTickets();
   }, []);
+  
+  if (loading) {
+    // return <h1 style={{minHeight:"70vh",}}>NO TICKET AVAILABLE</h1>
+    return <span className="loader"></span>;
+  }
 
   return (
-    <div className="dashboardMain" >
-      <div className="frame-position">
-        <img src={frame1} alt="" />
-        <div className="content-width">
-          <h2>Total Tickets</h2>
-          <h1>{count.Pending || 0  + count.Resolved || 0 + count.Open || 0}</h1>
+    <div className="dashboardMain">
+      <NavLink to={"/manager/openTickets"} className="menu">
+        <div className="frame-position">
+          <img src={frame1} alt="" />
+          <div className="content-width">
+            <h2>Open Tickets</h2>
+            <h1>{count.openCount || 0}</h1>
+          </div>
         </div>
-      </div>
-      <div className="frame-position">
-        <img src={frame2} alt="" />
-        <div className="content-width">
-          <h2>Resolved  and Closed Ticket</h2>
-          <h1>{count.Resolved || 0}</h1>
+      </NavLink>
+      <NavLink to={"/manager/pendingTickets"} className="menu">
+        <div className="frame-position">
+          <img src={frame2} alt="" />
+          <div className="content-width">
+            <h2>Pending Tickets</h2>
+            <h1>{count.pendingCount || 0}</h1>
+          </div>
         </div>
-      </div>
-      <div className="frame-position">
-        <img src={frame3} alt="" />
-        <div className="content-width" >
-           {/*Pending Ticket  */}
-          <h2>Open Tickets</h2>
-          <h1>{count.Open || 0}</h1>
+      </NavLink>
+      <NavLink to={"/manager/resolvedTickets"} className="menu">
+        <div className="frame-position">
+          <img src={frame3} alt="" />
+          <div className="content-width">
+            <h2>Resolved Tickets</h2>
+            <h1>{count.resolvedCount || 0}</h1>
+          </div>
         </div>
-      </div>
-      <div className="frame-position">
-        <img src={frame4} alt="" />
-        <div className="content-width" >
-          <h2>Open Since last Two Days</h2>
-          <h1>{count.OpenSinceLastTwoDays || 0}</h1>
+      </NavLink>
+      <NavLink to={"/manager/highPriorityTickets"} className="menu">
+        <div className="frame-position">
+          <img src={frame4} alt="" />
+          <div className="content-width">
+            <h2>High Priority Tickets</h2>
+            <h1>{count.HighPriorityCount || 0}</h1>{" "}
+            {/* Show 0 if count is undefined */}
+          </div>
         </div>
-      </div>
-      <div className="frame-position">
-        <img src={frame5} alt="" />
-        <div className="content-width">
-          <h2>Staff Counts</h2>
-          <h1>{count.staffCount || 0}</h1>
+      </NavLink>
+      <NavLink to={"/manager/midPriorityTickets"} className="menu">
+        <div className="frame-position">
+          <img src={frame5} alt="" />
+          <div className="content-width">
+            <h2>Mid Priority Tickets</h2>
+            <h1>{count.MidPriorityCount || 0}</h1>{" "}
+            {/* Show 0 if count is undefined */}
+          </div>
         </div>
-      </div>
+      </NavLink>
+      <NavLink to={"/manager/lowPriorityTickets"} className="menu">
+        <div className="frame-position">
+          <img src={frame5} alt="" />
+          <div className="content-width">
+            <h2>Low Priority Tickets</h2>
+            <h1>{count.LowPriorityCount || 0}</h1>{" "}
+            {/* Show 0 if count is undefined */}
+          </div>
+        </div>
+      </NavLink>
     </div>
   );
 };
+
 
 export default StaffDashboard;
